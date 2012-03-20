cea4dcfa242333c8dbcc544b5bcb0a5b33857330
Charles Miglietti <charles.miglietti@yahoo.fr>
2012-03-20 16:33:30 +0100

    separate logger module

--- server-restify.js
+++ server-restify.js
@@ -5,39 +5,11 @@
 
 
 var restify = require('restify'),
-    // Winston is a logging module - part of Flatiron
-    winston = require('winston'),
-    // Config object for logger
-    logger_config = {
-                      "levels" : {
-                        "detail": 0,
-                        "trace": 1,
-                        "debug": 2,
-                        "enter": 3,
-                        "info": 4,
-                        "warn": 5,
-                        "error": 6
-                      },
-                      "colors" : {
-                        "detail": "grey",
-                        "trace": "white",
-                        "debug": "blue",
-                        "enter": "inverse",
-                        "info": "green",
-                        "warn": "yellow",
-                        "error": "red"
-                      } 
-                    },
+    // Custom logger built with Winston
+    logger = require('./logger.js'),
     // Instantiate server from restify
     server = restify.createServer({
       name: 'Tldr-proto',
-    }),
-    // Instantiate Logger
-    logger = new(winston.Logger)({
-      transports: [new (winston.transports.Console)({
-                          colorize:true}),],
-      levels: logger_config.levels,
-      colors: logger_config.colors,
     }),
     // Port to connect to server
     PORT = 8787;
@@ -47,6 +19,11 @@ logger.debug('Test Debug');
 logger.info('Test info');
 logger.error('Test error');
 
+//Hanlding annoying favicon request in Chrome
+server.get('/favicon.ico', function(req, res, next){
+  logger.info('querying favicon');
+  res.end();
+});
 
 // Start server
 server.listen(PORT, function(){
