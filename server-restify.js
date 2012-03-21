2f4a148030ddbca5db0caa5ecad24d0d88a00550
Charles Miglietti <charles.miglietti@yahoo.fr>
2012-03-21 14:38:48 +0100

    testing with restify

--- server-restify.js
+++ server-restify.js
@@ -12,18 +12,44 @@ var restify = require('restify'),
     // Instantiate server from restify
     server = restify.createServer(),
     // Port to connect to server
-    PORT = 8787;
+    config = require('./lib/config.js'),
+		PORT = config.PORT_DEV;
 
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
 
-
-// Start server
-server.listen(PORT, function(){
-  logger.info('Server launched');
+server.get('/hello', function(req, res, next) {
+  res.send('hello ');
+  logger.info('handle hello request');
+});
+server.get('/test', function(req, res, next) {
+  res.send('test');
+  logger.info('handle test request');
 });
 
+server.get('/', function(req, res, next) {
+  res.send('test');
+  logger.info('handle test request');
+});
+
+// Start server
+if (module.parent === null) {
+	server.listen(PORT, function(){
+		logger.info('Server launched at '+ server.url);
+	});
+}
+
+//var client = restify.createJsonClient({
+  //url: 'http://localhost:' + PORT,
+//});
+
+//client.get('/hello', function(err, req, res, obj) {
+
+		//console.log('in client');
+		//console.log(JSON.stringify(obj, null, 2));
+//});
 
 
+module.exports = server;
--- test/test.server.js
+++ test/test.server.js
@@ -1,5 +1,18 @@
 var should = require('chai').should(),
-    assert = require('chai').assert;
+    assert = require('chai').assert,
+    restify = require('restify'),
+    server = require('../server-restify.js'),
+    config = require('../lib/config.js'),
+    PORT = config.PORT_TEST;
+
+
+server.listen(PORT, function() {
+  console.log('listening ' + server.url);
+});
+server.get('/test', function(req, res, next) {
+  res.send('test');
+  console.log('handle hello request');
+});
 
 describe('This test', function() {
   it('should be an very useful test', function() {
@@ -18,3 +31,21 @@ describe('This test', function() {
     name.should.equal(post.author);
   });
 });
+
+
+describe('Set up env: ', function() {
+  it('should return hello', function() {
+    var client = restify.createJsonClient({
+      url: 'http://localhost:' + config.PORT_DEV,
+    });
+    console.log('client' + client.url);
+    client.get('/test', function(err, req, res, obj) {
+        obj.should.equal('hello');
+        console.log('in client');
+        //console.log(JSON.stringify(obj, null, 2));
+    });
+  });
+});
+
+
+
