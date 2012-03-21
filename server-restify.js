38894479e0ff133c7378bc4d8dd44eddd685031f
Charles Miglietti <charles.miglietti@yahoo.fr>
2012-03-21 17:56:55 +0100

    first get test

--- server-restify.js
+++ server-restify.js
@@ -4,6 +4,9 @@
  */
 
 
+/***********************************/
+/* Require dependencies            */
+/***********************************/
 var restify = require('restify'),
     // Custom logger built with Winston
     winston = require('./lib/logger.js').winston,
@@ -16,20 +19,48 @@ var server = restify.createServer(),
 		PORT = config.PORT_DEV;
 
 
+/***********************************/
+/* Bundle Plugins Restify          */
+/***********************************/
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
+/********************/
+/* Routing          */
+/********************/
 
-server.get('/hello', function(req, res, next) {
-  res.send('hello toto');
-  winston.info('handle hello request');
+
+// Full tldrs list
+server.get('/tldrs', function (req, res, next) {
+  res.send(403, 'Dont dump the db fucking idiot');
+});
+
+/**
+ * Get tldr identified by one id
+ * @method {/tldrs/:id}
+ * @return {Tldr Object} Returns the Tldr object corresponding to the
+ *											 required id
+ */
+
+
+server.get('/tldrs/:id', function (req, res, next) {
+	var tldr = {id: req.id, 
+						  url:'needforair.com/Mofo',
+							summary:'* Programming Mofo  '+
+											'* Programming Mofo',
+	};
+  res.send(tldr);
 });
 
 
+/********************/
+/* Start server     */
+/********************/
+
 // Start server
 if (module.parent === null) {
-	server.listen(PORT, function(){
+	server.listen(PORT, function (){
 		winston.info('Server launched at '+ server.url);
 	});
 }
--- test/test.server.js
+++ test/test.server.js
@@ -1,3 +1,7 @@
+/***********************************/
+/* Require dependencies            */
+/***********************************/
+
 var should = require('chai').should(),
     assert = require('chai').assert,
     restify = require('restify'),
@@ -5,24 +9,29 @@ var should = require('chai').should(),
     config = require('../lib/config.js'),
     winston = require('../lib/logger.js').winston;
 
-var client = restify.createJsonClient({
+/***********************************/
+/* Global setup                    */
+/***********************************/
+var PORT = config.PORT_TEST,
+    client = restify.createJsonClient({
         url: 'http://localhost:' + PORT,
-    }),
-    PORT = config.PORT_TEST;
+    });
 
-server.listen(PORT, function() {
+server.listen(PORT, function () {
   winston.info('listening ' + server.url);
 });
 
-// Test Example
-describe('This test', function() {
-  it('should be an very useful test', function() {
+/***********************************/
+/* Temporary test example          */
+/***********************************/
+describe('This test', function () {
+  it('should be an very useful test', function () {
     var a = 4,
         b = 5,
         c = a + b;
     assert.equal(c, 9);
   });
-  it('should be even more useful ', function() {
+  it('should be even more useful ', function () {
     var name = 'NFA',
         post = {date:'1/1/1111', 
                 author:'NFA',
@@ -34,17 +43,40 @@ describe('This test', function() {
 });
 
 
-describe('Set up env: ', function() {
+/***********************************/
+/* Tests                           */
+/***********************************/
+
+// Test for non existent path and not allowed calls 
+
+describe('Global behaviour ', function () {
   // The done arg is very important ! If absent tests run synchronously
   // that means there is n chance you receive a response to your request
   // before mocha quits 
-  it('should return hello', function(done) {
-    client.get('/hello', function(err, req, res, obj) {
-        winston.info(JSON.stringify(obj, null, 2));
+  it('should be a 404 page', function (done) {
+    client.get('/badroute', function (err, req, res, obj) {
+        res.statusCode.should.equal(404);
+        done();
+    });
+  });
+
+  it('should not be allowed to dump full db', function (done) {
+    client.get('/tldrs', function (err, req, res, obj) {
+        res.statusCode.should.equal(403);
         done();
     });
   });
 });
 
+// Test simple GET
+
+describe('Get Requests', function () {
+  it('basic query with id', function (done) {
+    client.get('/tldrs/1', function (err, req, res, obj) {
+      obj.should.have.property('summary');
+      done();
+    });
+  });
+});
 
 
