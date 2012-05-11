6e058aadd2f455d35046bf9f74f0a3df513a0c5e
Charles Miglietti <charles.miglietti@yahoo.fr>
2012-05-11 12:24:11 +0200

    test for cleanurl

--- test/models_tldrModel.test.js
+++ test/models_tldrModel.test.js
@@ -28,22 +28,29 @@ var should = require('chai').should()
 
 describe('TldrModel', function () {
 
+  before(function (done) {
+    db.connectToDatabase(done);
+  });
+
+  after(function (done) {
+    db.closeDatabaseConnection(done);
+  });
+
+  beforeEach(function (done) {
+    TldrModel.remove( function (err) {
+      if (err) {throw done(err);}
+      done();
+    });
+  });
+
+
+  describe('#setters', function () {
+    it('should clean the url provided', function (done) {
+      done();
+    });
+  });
+  
   describe('#validators', function () {
-
-    before(function (done) {
-      db.connectToDatabase(done);
-    });
-
-    after(function (done) {
-      db.closeDatabaseConnection(done);
-    });
-
-    beforeEach(function (done) {
-      TldrModel.remove( function (err) {
-        if (err) {throw done(err);}
-        done();
-      });
-    });
 
     it('should detect missing required _id arg', function (done) {
 
@@ -208,7 +215,7 @@ describe('TldrModel', function () {
   describe('#createInstance', function () {
 
 
-    it ('should calculate the correct id and hostname', function() {
+    it ('should compute the correct id and hostname', function() {
 
       var tldr = TldrModel.createInstance({url: 'http://adomain.tld',
                                           title: 'Learn to Code',
@@ -218,19 +225,38 @@ describe('TldrModel', function () {
 
       tldr.hostname.should.equal('adomain.tld');
       tldr.title.should.equal('Learn to Code');
-      tldr._id.should.equal('839f9a097256c214581548a39bb7840a27c242e2');
+      tldr._id.should.equal('4db23ed1d1d6b4ebf43d668024f39d759ec7c157');
 
+    });
+
+    it('should clean url and keep hostname+path only', function () {
+      var tldr = TldrModel.createInstance({url: 'http://mydomain.com?toto=tata&titi=tutu',
+                                          title: 'Some Title',
+                                          summary: 'Summary is good',
+                                          resourceAuthor: 'John'});
+
+      tldr.url.should.equal('http://mydomain.com/');
+      tldr._id.should.equal('2a179becf9a98250889e3812e167771303fa195d');
+
+      tldr = TldrModel.createInstance({url: 'http://mydomain.com#anchor',
+                                          title: 'Some Title',
+                                          summary: 'Summary is good',
+                                          resourceAuthor: 'John'});
+
+      tldr.url.should.equal('http://mydomain.com/');
+      tldr._id.should.equal('2a179becf9a98250889e3812e167771303fa195d');
     });
     
 
     it('should allow user to set url, summary and resourceAuthor only', function () {
 
       // Test is coupled with createInstance because they are designed to work together
-      var tldr = TldrModel.createInstance({url: 'bla'
+      var tldr = TldrModel.createInstance({url: 'http://mydomain.com'
                                           , title: 'Blog NFA'
                                           , summary: 'coin'
                                           , resourceAuthor: 'bloup'});
-      tldr.url.should.equal('bla');
+
+      tldr.url.should.equal('http://mydomain.com/');
       tldr.summary.should.equal('coin');
       tldr.resourceAuthor.should.equal('bloup');
 
@@ -238,23 +264,23 @@ describe('TldrModel', function () {
       tldr2.should.not.have.property('unusedField');
       tldr2.should.not.have.property('summary');
       tldr2.should.not.have.property('resourceAuthor');
-      tldr2.url.should.eql('');
+      tldr2.url.should.eql('http://nonexistingdomain.com/');
 
     });
 
     it('should restrict the fields the user is allowed to update', function () {
 
-      var tldr = TldrModel.createInstance({url: 'bla'
+      var tldr = TldrModel.createInstance({url: 'http://mydomain.com'
                                           , title: 'Blog NFA'
                                           , summary: 'coin'
                                           , resourceAuthor: 'bloup'})
-        , toUpdate = {url: 'new1'
+        , toUpdate = {url: 'http://myotherdomain.com'
           , summary: 'new2'
           , title: 'Blog NeedForAir'
           , resourceAuthor: 'new3'
           , unusedField: 'new4'};
 
-      tldr.url.should.equal('bla');
+      tldr.url.should.equal('http://mydomain.com/');
       tldr.summary.should.equal('coin');
       tldr.title.should.equal('Blog NFA');
       tldr.resourceAuthor.should.equal('bloup');
@@ -262,7 +288,7 @@ describe('TldrModel', function () {
       // Perform update
       tldr.update(toUpdate);
 
-      tldr.url.should.equal('bla');
+      tldr.url.should.equal('http://mydomain.com/');
       tldr.summary.should.equal('new2');
       tldr.title.should.equal('Blog NeedForAir');
       tldr.resourceAuthor.should.equal('new3');
