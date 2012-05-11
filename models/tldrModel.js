e992868fbd277a87e52c4ea89a25f568b67d8433
Stanislas Marion <stanislas.marion@gmail.com>
2012-05-11 23:05:33 +0200

    cherry picking conflit resolved

--- models/tldrModel.js
+++ models/tldrModel.js
@@ -11,8 +11,8 @@ var mongoose = require('mongoose')
   , Schema = mongoose.Schema
   , TldrSchema
   , TldrModel
-  , userSetableFields = ['_id','summary','title', 'resourceAuthor'] // setable fields by user
-  , userUpdatableFields = ['summary', 'title', 'resourceAuthor'];// updatabe fields by user
+  , userSetableFields = ['_id', 'summary','title', 'resourceAuthor', 'resourceDate'] // setable fields by user
+  , userUpdatableFields = ['summary', 'title', 'resourceAuthor', 'resourceDate'];// updatabe fields by user
 
 
 
