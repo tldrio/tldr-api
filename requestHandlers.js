a1932deb5a3a622b2b1723b4044527f3d6bf1ea6
Louis Chatriot <louis.chatriot@gmail.com>
2012-05-24 10:26:54 +0200

    Dichotomy test

--- requestHandlers.js
+++ requestHandlers.js
@@ -25,7 +25,7 @@ function handleInternalDBError(err, next, msg) {
 
 // GET all tldrs
 function getAllTldrs (req, res, next) {
-    return next(new restify.NotAuthorizedError('Dumping the full tldrs db is not allowedTTTTTTT'));
+    return next(new restify.NotAuthorizedError('Dumping the full tldrs db is not allowedUUUUUUUUUUU'));
 }
 
 // GET a tldr by url
