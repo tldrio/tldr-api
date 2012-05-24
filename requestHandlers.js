44a1522e9401274a190bb549c9e30d398c83273e
Louis Chatriot <louis.chatriot@gmail.com>
2012-05-24 10:33:33 +0200

    Dicho always, 1 box to go

--- requestHandlers.js
+++ requestHandlers.js
@@ -25,7 +25,7 @@ function handleInternalDBError(err, next, msg) {
 
 // GET all tldrs
 function getAllTldrs (req, res, next) {
-    return next(new restify.NotAuthorizedError('Dumping the full tldrs db is not allowedMMMMM'));
+    return next(new restify.NotAuthorizedError('Dumping the full tldrs db is not allowedYUYUYUHHHH'));
 }
 
 // GET a tldr by url
