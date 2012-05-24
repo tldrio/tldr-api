e6eeb9be5f2986bf18245a749c21f830866e7b1d
Louis Chatriot <louis.chatriot@gmail.com>
2012-05-24 10:22:17 +0200

    Fucking again

--- requestHandlers.js
+++ requestHandlers.js
@@ -25,7 +25,7 @@ function handleInternalDBError(err, next, msg) {
 
 // GET all tldrs
 function getAllTldrs (req, res, next) {
-    return next(new restify.NotAuthorizedError('Dumping the full tldrs db is not allowedYYYYY ZZZ'));
+    return next(new restify.NotAuthorizedError('Dumping the full tldrs db is not allowedFFFFFFF'));
 }
 
 // GET a tldr by url
