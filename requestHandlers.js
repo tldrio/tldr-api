b50da672e5da58adefd944a8f4539cb3c9094279
Charles Miglietti <charles.miglietti@yahoo.fr>
2012-06-20 11:54:07 +0200

    CORS headers

--- requestHandlers.js
+++ requestHandlers.js
@@ -252,7 +252,8 @@ function handleErrors (err, req, res, next) {
 
 function allowAccessOrigin (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
   next();
 }
 
