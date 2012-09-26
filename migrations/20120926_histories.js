ccf9d79594e24a5a745ba6f1ba4ff8c7ce7a6168
Louis Chatriot <louis.chatriot@gmail.com>
2012-09-26 11:16:42 +0200

    Stub for migration with connect disconnect ok

--- migrations/20120926_histories.js
+++ migrations/20120926_histories.js
@@ -6,3 +6,31 @@
  */
 
 var async = require('async')
+  , DbObject = require('../lib/db')
+  , config = require('../lib/config')
+  , db = new DbObject( config.dbHost
+                     , config.dbName
+                     , config.dbPort
+                     );
+
+console.log('===== Beginning migration =====');
+console.log('Connecting to DB ' + config.dbHost + ':' + config.dbPort + '/' + config.dbName)
+
+async.waterfall([
+  function (cb) {
+    db.connectToDatabase(function() {
+      console.log('Connected');
+      cb();
+    });
+  }
+, function (cb) {
+    console.log("Do something");
+    cb();
+  }
+, function (cb) {
+    db.closeDatabaseConnection(function () {
+      console.log("Closed connection to database");
+      process.exit(0);
+    });
+  }
+]);
