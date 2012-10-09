8cf0a5dff5a1296f5e355daca14508cebe58c14c
Louis Chatriot <louis.chatriot@gmail.com>
2012-10-09 12:47:24 +0200

    tldrs created now generated server side - and thats fucking much better

--- routes/website/tldrscreated.js
+++ routes/website/tldrscreated.js
@@ -9,10 +9,13 @@ module.exports = function (req, res, next) {
   var values = {};
 
   values.user = req.user;
+  values.user.getCreatedTldrs(function(tldrs) {
+    values.tldrsCreated = tldrs;
 
-  res.render('website/basicLayout', { values: values
-                                    , partials: { content: '{{>website/pages/tldrscreated}}' }
-                                    });
+    res.render('website/basicLayout', { values: values
+               , partials: { content: '{{>website/pages/tldrscreated}}' }
+    });
+  });
 }
 
 
