2f23bdb1fc7bb18ec9b405438221d4b23303168c
Louis Chatriot <louis.chatriot@gmail.com>
2012-10-15 18:10:38 +0200

    coherent with adminOnly

--- server.js
+++ server.js
@@ -142,8 +142,11 @@ app.get('/forgotPassword', routes.website_forgotPassword);
 app.get('/resetPassword', routes.website_resetPassword);
 
 // Private pages
-app.get('/account', middleware.loggedInOnly(routes.website_account));
-app.get('/tldrscreated', middleware.loggedInOnly(routes.website_tldrscreated));
+app.get('/account', middleware.loggedInOnly, routes.website_account);
+app.get('/tldrscreated', middleware.loggedInOnly, routes.website_tldrscreated);
+
+
+
 
 /*
  * Compile all templates and partials, connect to database, then start server
