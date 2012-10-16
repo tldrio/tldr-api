a63f6d877aa600b57985d2f97219deec917e51eb
Louis Chatriot <louis.chatriot@gmail.com>
2012-10-16 09:55:26 +0200

    Basic backend functionality for the user profile

--- routes/website/userPublicProfile.js
+++ routes/website/userPublicProfile.js
@@ -4,15 +4,24 @@
  * Proprietary License
 */
 
+var models = require('../../lib/models')
+  , User = models.User;
+
 
 module.exports = function (req, res, next) {
-  var values = {};
+  var values = {}
+    , usernameLowerCased = req.params.username ? req.params.username.toLowerCase() : '';
 
-  console.log(req.params.username);
   values.user = req.user;
 
-  res.render('website/basicLayout', { values: values
-                                    , partials: { content: '{{>website/pages/userPublicProfile}}' }
-                                    });
+  User.findOne({ usernameLowerCased: usernameLowerCased })
+      .populate('tldrsCreated')
+      .exec(function (err, user) {
+              if (err || !user) { values.userNotFound = true; }
+
+              res.render('website/basicLayout', { values: values
+                                                , partials: { content: '{{>website/pages/userPublicProfile}}' }
+                                                });
+            });
 }
 
