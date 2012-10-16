d0171aa3d0f8cb1af2ea59bb7fd447541a58a29c
Louis Chatriot <louis.chatriot@gmail.com>
2012-10-16 12:45:54 +0200

    Latest tldrs created shown, title appears only if there are some

--- routes/website/userPublicProfile.js
+++ routes/website/userPublicProfile.js
@@ -17,22 +17,26 @@ module.exports = function (req, res, next) {
   values.loggedUser = req.user;
 
   async.waterfall([
-    function (cb) {
+    function (cb) {   // Get user info. For now, reputation is the number of created tldrs
       User.findOne({ usernameLowerCased: usernameLowerCased })
           .exec(function (err, user) {
-            if (err || !user) { values.userNotFound = true; }
-
-            values.user = user;
-            values.user.createdAtReadable = customUtils.dateForDisplay(user.createdAt);
-
+            if (err || !user) {
+              values.userNotFound = true;
+            } else {
+              values.user = user;
+              values.user.createdAtReadable = customUtils.dateForDisplay(user.createdAt);
+              values.user.createdMoreThanOneTldr = (user.tldrsCreated.length > 0);
+            }
             cb();
           });
     }
-  , function (cb) {
+  , function (cb) {   // Only populate the latest tldrs the user created, in a specific object
       User.findOne({ usernameLowerCased: usernameLowerCased })
-          .populate('tldrsCreated', '_id title')
+          .populate('tldrsCreated', '_id title', {}, { limit: 5, sort: [['createdAt', -1]] })
           .exec(function (err, user) {
-            values.tldrsCreatedToDisplay = user.tldrsCreated;
+            if (! err && user) {
+              values.tldrsCreatedToDisplay = user.tldrsCreated;
+            }
 
             cb();
           });
