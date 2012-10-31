267d9aee68901ef3b290f34573853831aaedc91c
Louis Chatriot <louis.chatriot@gmail.com>
2012-10-31 17:11:09 +0100

    Voting buttons work

--- routes/website/forum.js
+++ routes/website/forum.js
@@ -20,6 +20,11 @@ module.exports = function (req, res, next) {
     _.each(topics, function (topic) {
        topic.lastPostTimeago = customUtils.timeago(topic.lastPost.at);
        topic.moreThanOnePost = (topic.posts.length > 1);
+       topic.moreThanOneVote = (topic.votes > 1) || (topic.votes === 0) || (topic.votes < -1);
+
+       if (req.user) {   // Won't display the buttons if nobody's logged in
+         topic.userHasntVoted = topic.alreadyVoted.indexOf(req.user._id) === -1;
+       }
      });
 
     res.render('website/basicLayout', { values: values
