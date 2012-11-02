f4fe15dbea3b428e6b12f3af72caca868b78dc09
Louis Chatriot <louis.chatriot@gmail.com>
2012-11-02 15:45:18 +0100

    Whitespace

--- routes/website/tldrPage.js
+++ routes/website/tldrPage.js
@@ -20,6 +20,4 @@ module.exports = function (req, res, next) {
                                       , partials: { content: '{{>website/pages/tldrPage}}' }
                                       });
   });
-
 }
-
