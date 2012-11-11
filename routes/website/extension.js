196d6cb589722396943f8fbd010ccaf745133da2
Stanislas Marion <stanislas.marion@gmail.com>
2012-11-11 21:29:49 +0100

    chrome sniffing

--- routes/website/extension.js
+++ routes/website/extension.js
@@ -6,7 +6,12 @@
 
 
 module.exports = function (req, res, next) {
-  var values = req.renderingValues;
+  var values = req.renderingValues
+    , ua = req.headers['user-agent']
+    , chrome = ua.match(/Chrome/g)
+    ;
+
+  values.chrome = chrome;
 
   res.render('website/basicLayout', { values: values
                                     , partials: { content: '{{>website/pages/extension}}' }
