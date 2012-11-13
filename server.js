dd40b98b931fba0ef239722e670e5a87acfaec7e
Louis Chatriot <louis.chatriot@gmail.com>
2012-11-13 19:39:55 +0100

    fix url bug

--- server.js
+++ server.js
@@ -160,6 +160,7 @@ app.get('/signup', middleware.attachRenderingValues, routes.website_signup);
 app.get('/tldrs', middleware.attachRenderingValues, routes.website_tldrs);
 app.get('/whatisit', middleware.attachRenderingValues, routes.website_whatisit);
 app.get('/chromeextension', middleware.attachRenderingValues, routes.website_extension);
+app.get('/extension', function (req, res, next) { res.redirect(config.websiteUrl + '/chromeextension'); });
 
 // Login, logout
 app.get('/logout', function (req, res, next) { req.logOut(); res.redirect(config.websiteUrl + '/index'); });
