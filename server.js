57ed1321cbff737e1b216216ecf7c24e1db1a6bd
Louis Chatriot <louis.chatriot@gmail.com>
2012-11-13 20:36:49 +0100

    Better fix

--- server.js
+++ server.js
@@ -159,8 +159,8 @@ app.get('/index', middleware.attachRenderingValues     // Routing for this page 
 app.get('/signup', middleware.attachRenderingValues, routes.website_signup);
 app.get('/tldrs', middleware.attachRenderingValues, routes.website_tldrs);
 app.get('/whatisit', middleware.attachRenderingValues, routes.website_whatisit);
-app.get('/chromeextension', middleware.attachRenderingValues, routes.website_extension);
-app.get('/extension', function (req, res, next) { res.redirect(config.websiteUrl + '/chromeextension'); });
+app.get('/chromeextension', middleware.attachRenderingValues, routes.website_extension);   // Fuck us
+app.get('/extension', middleware.attachRenderingValues, routes.website_extension);
 
 // Login, logout
 app.get('/logout', function (req, res, next) { req.logOut(); res.redirect(config.websiteUrl + '/index'); });
