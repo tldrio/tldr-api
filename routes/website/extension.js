4d438c25584cd71b15bbdb41ea02767b73287e92
Charles Miglietti <charles.miglietti@yahoo.fr>
2012-11-14 15:53:37 +0100

    A/B test landing

--- routes/website/extension.js
+++ routes/website/extension.js
@@ -9,10 +9,18 @@ module.exports = function (req, res, next) {
   var values = req.renderingValues
     , ua = req.headers['user-agent']
     , chrome = ua.match(/Chrome/g)
+    , AB = Math.floor( Math.random() * 2)
     ;
 
   values.extension = true;
   values.chrome = chrome;
+  if (AB) {
+    values.versionA = true;
+    values.version = 'A';
+  } else {
+    values.versionB = true;
+    values.version = 'B';
+  }
 
   res.render('website/basicLayout', { values: values
                                     , partials: { content: '{{>website/pages/extension}}' }
