54fad6f67a344babccf4e909c5daedc19c9cac3e
Louis Chatriot <louis.chatriot@gmail.com>
2012-11-22 16:38:15 +0100

    Added a migration test

---
+++ migrations/20121122_url_normalization_test.js
@@ -0,0 +1,63 @@
+/*
+ * Migration to test which tldr urls would change and how with the new normalization, so as to to avoid bad surprise
+ * Date: Nov 22nd, 2012
+ *
+ */
+
+var async = require('async')
+  , _ = require('underscore')
+  , models = require('../lib/models')
+  , Tldr = models.Tldr
+  , DbObject = require('../lib/db')
+  , customUtils = require('../lib/customUtils')
+  , config = require('../lib/config')
+  , db = new DbObject( config.dbHost
+                     , config.dbName
+                     , config.dbPort
+                     );
+
+console.log('===== Beginning migration =====');
+console.log('Connecting to DB ' + config.dbHost + ':' + config.dbPort + '/' + config.dbName)
+
+async.waterfall([
+  function (cb) {
+    db.connectToDatabase(function() {
+      console.log('Connected');
+      cb();
+    });
+  }
+
+  // Add the versionDisplayed field
+, function (cb) {
+    var i = 0, tu;
+
+    console.log("Checking urls vs their normalization");
+
+    Tldr.find({ }, function(err, tldrs) {
+      if (err) { return cb(err); }
+
+      async.whilst(
+        function () { return i < tldrs.length; }
+      , function (cb) {
+          tu = tldrs[i].url;
+
+          if (tu !== customUtils.normalizeUrl(tu)) {
+            console.log('' + tldrs[i]._id + ": " + tu + " - vs - " + customUtils.normalizeUrl(tu));
+          }
+
+          cb();
+        }
+      , cb);
+    });
+  }
+], function (err) {
+    if (err) {
+      console.log('Something unexpected occured, stopped migration. ', err);
+    }
+
+    db.closeDatabaseConnection(function () {
+      console.log("Closed connection to database");
+      process.exit(0);
+    });
+  }
+);
