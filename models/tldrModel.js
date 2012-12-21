3f4c6fa91952fd63bee384d3ca33e0e820311041
Louis Chatriot <louis.chatriot@gmail.com>
2012-12-21 10:39:34 +0100

    Removed useless type from tldr read event

--- models/tldrModel.js
+++ models/tldrModel.js
@@ -219,8 +219,7 @@ TldrSchema.statics.findAndIncrementReadCount = function (selector, user, callbac
   query.exec( function (err, tldr) {
     if (!err && tldr) {
       // Send Notif
-      rqClient.emit('tldr.read', { type: 'read'
-                                 , from: user
+      rqClient.emit('tldr.read', { from: user
                                  , tldr: tldr
                                  // all contributors instead of creator only ?? we keep creator for now as there a very few edits
                                  , to: tldr.creator
