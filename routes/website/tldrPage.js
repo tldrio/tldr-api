0cd6542866eed3054efe9b9dc70f0b7f80cc4e1b
Louis Chatriot <louis.chatriot@gmail.com>
2012-12-28 12:23:17 +0100

    Remove unused comments

--- routes/website/tldrPage.js
+++ routes/website/tldrPage.js
@@ -6,7 +6,9 @@
 
 var _ = require('underscore')
   , Tldr = require('../../lib/models').Tldr
-  , bunyan = require('../../lib/logger').bunyan;
+  , bunyan = require('../../lib/logger').bunyan
+  , config = require('../../lib/config')
+  ;
 
 module.exports = function (req, res, next) {
   var values = req.renderingValues || {}
@@ -17,23 +19,21 @@ module.exports = function (req, res, next) {
   partials.content = '{{>website/pages/tldrPage}}';
   partials.fbmetatags = '{{>website/metatags/metatagsPage}}'
 
-  // After the tldr has been found (either by its slug or _id), render it
-  function renderTldrPage(tldr) {
-    values = _.extend(values, tldr);
-
-    return res.render('website/basicLayout', { values: values
-                                             , partials: partials
-                                             });
-  }
-
   // First, try to find the tldr by the slug
   Tldr.findAndIncrementReadCount({ slug: req.params.id }, req.user, function (err, tldr) {
-    if (!err && tldr) { return renderTldrPage(tldr); }
+    if (!err && tldr) {
+      values = _.extend(values, tldr);
+      return res.render('website/basicLayout', { values: values , partials: partials });
+    }
 
     // If it can't be found by the slug, try the _id for retrocompatibility
-    Tldr.findAndIncrementReadCount({ _id: req.params.id }, req.user, function (err, tldr) {
-      if (err || !tldr) { values.tldrNotFound = true; }
-      return renderTldrPage(tldr);
+    Tldr.findOne({ _id: req.params.id }, function (err, tldr) {
+      if (err || !tldr || !tldr.slug || tldr.slug.length === 0) {
+        values.tldrNotFound = true;
+        return res.render('website/basicLayout', { values: values , partials: partials });
+      }
+
+      return res.redirect(301, config.websiteUrl + '/tldrs/' + tldr.slug);
     });
   });
 }
