930b05acb9ec00e01913884fc69b012aeecaadca
Louis Chatriot <louis.chatriot@gmail.com>
2013-01-13 20:21:29 +0100

    Use markdown to get a more Github-like feel for the doc

--- routes/website/apiDoc.js
+++ routes/website/apiDoc.js
@@ -9,11 +9,13 @@ var config = require('../../lib/config')
 module.exports = function (req, res, next) {
   var partials = req.renderingPartials || {}
     , values = req.renderingValues || {}
+    , marked = require('../../lib/customMarked')
     ;
 
   values.title = "Developpers" + config.titles.branding + config.titles.shortDescription;
   values.description = "Documentation for our API.";
   partials.content = '{{>website/pages/apiDoc}}';
+  values.markdown = function (text) { return marked(text); };   // Lambda to use markdown
 
   res.render('website/basicLayout', { values: values
                                     , partials: partials
