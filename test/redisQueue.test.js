efa5216aa45150b1c807b23ac292291b714f5782
Louis Chatriot <louis.chatriot@gmail.com>
2013-02-04 18:13:01 +0100

    Redis Queue can now do both actions with the same client at fucking last

--- test/redisQueue.test.js
+++ test/redisQueue.test.js
@@ -9,41 +9,37 @@ var should = require('chai').should()
   , assert = require('chai').assert
   , _ = require('underscore')
   , i18n = require('../lib/i18n')
-  , rqConfig = { port: 6379, db: 2, scope: 'bloup' }
+  , rqConfig = { port: 6379, scope: 'bloup' }
   , RedisQueue = require('../lib/redis-queue')
   ;
 
 
-describe('Redis Queue', function () {
+describe.only('Redis Queue', function () {
 
   it('Should send and receive standard messages correctly', function (done) {
-    var rq1 = new RedisQueue(rqConfig)
-      , rq2 = new RedisQueue(rqConfig)
-      ;
+    var rq = new RedisQueue(rqConfig);
 
-    rq1.on('un test', function (data) {
+    rq.on('un test', function (data) {
       data.first.should.equal('First message');
       data.second.should.equal('Second message');
       done();
     }
     , function () {
-        rq2.emit('un test', { first: 'First message'
+        rq.emit('un test', { first: 'First message'
                             , second: 'Second message' });
       });
   });
 
   it('Should receive pattern messages correctly', function (done) {
-    var rq1 = new RedisQueue(rqConfig)
-      , rq2 = new RedisQueue(rqConfig)
-      ;
+    var rq = new RedisQueue(rqConfig);
 
-    rq1.on('test:*', function (data) {
+    rq.on('test:*', function (data) {
       data.first.should.equal('First message');
       data.second.should.equal('Second message');
       done();
     }
     , function () {
-        rq2.emit('test:created', { first: 'First message'
+        rq.emit('test:created', { first: 'First message'
                             , second: 'Second message' });
       });
   });
