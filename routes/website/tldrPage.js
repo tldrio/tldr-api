1ba851b50e33d9960cefd50e36fddeb248827215
Louis Chatriot <louis.chatriot@gmail.com>
2013-02-04 11:37:48 +0100

    Added Summary of in the title of tldr pages for SEO purposes

--- routes/website/tldrPage.js
+++ routes/website/tldrPage.js
@@ -30,8 +30,9 @@ module.exports = function (req, res, next) {
     }
 
     values.tldr = tldr;
-    values.title = tldr.title.substring(0, 60) +
-                   (tldr.title.length > 60 ? '...' : '') +
+    values.title = 'Summary of "' +
+                   tldr.title.substring(0, 60) +
+                   (tldr.title.length > 60 ? '..."' : '"') +
                    config.titles.branding + config.titles.shortDescription;
     // Warning: don't use double quotes in the meta description tag
     values.description = "Summary written by " + tldr.creator.username + " of '" + tldr.title.replace(/"/g, '') + "'";
