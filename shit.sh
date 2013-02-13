97eb0b959c39acae4f7977a3d67843ecbcb174ee
Louis Chatriot <louis.chatriot@gmail.com>
2013-02-13 16:59:44 +0100

    Solved crazy bug

--- shit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#NODE_ENV=test ./node_modules/.bin/mocha test/apiClientModel.test.js test/customMarked.test.js test/customUtils.test.js test/models.test.js test/models_credentialsModel.test.js test/models_tldrHistory.test.js test/models_tldrModel.test.js test/models_userHistory.test.js test/models_userModel.test.js test/post.test.js test/redisQueue.test.js test/server.test.js --reporter spec
-
-NODE_ENV=test ./node_modules/.bin/mocha test/redisQueue.test.js test/server.test.js --reporter spec
-
