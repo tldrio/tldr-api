5b83e71e5d25ca5130e87007e0e62d00ffbb565a
Stanislas Marion <stanislas.marion@gmail.com>
2013-02-17 22:15:01 +0100

    fix

--- routes/website/index.js
+++ routes/website/index.js
@@ -9,18 +9,19 @@ var config = require('../../lib/config')
   ;
 
 module.exports = function (req, res, next) {
-  var values = req.renderingValues || {}
-    , partials = req.renderingPartials || {};
+  app.getTotalTldrReadCount(function (err, totalReadCount) {
+    var values = req.renderingValues || {}
+      , partials = req.renderingPartials || {};
 
-  values.index = true;
-  values.rssFeedPromotionLink = true;
-  values.totalReadCount = app.getTotalTldrReadCount(function () {});
-  console.log('OWYFGUWFYGIF', values.totalReadCount);
-  values.title = "tldr.io" + config.titles.shortDescription;
-  values.description = "Save time and discover great content by reading and writing summaries of the best of the web.";
-  partials.content = '{{>website/pages/index}}';
+    values.index = true;
+    values.rssFeedPromotionLink = true;
+    values.totalReadCount = totalReadCount;
+    values.title = "tldr.io" + config.titles.shortDescription;
+    values.description = "Save time and discover great content by reading and writing summaries of the best of the web.";
+    partials.content = '{{>website/pages/index}}';
 
-  res.render('website/basicLayout', { values: values
-                                    , partials: partials
-                                    });
+    res.render('website/basicLayout', { values: values
+                                      , partials: partials
+                                      });
+  });
 }
