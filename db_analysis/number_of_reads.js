68a3ba6126e0c80c2e64f1b84ac58518588603b8
Louis Chatriot <louis.chatriot@gmail.com>
2013-02-25 11:34:27 +0100

    New script to get all read

--- db_analysis/number_of_reads.js
+++ db_analysis/number_of_reads.js
@@ -1,31 +1,5 @@
-#!/usr/local/bin/node
+#!/bin/bash
 
-/*
- * Calculate the total number of reads
- *
- */
-
-var async = require('async')
-  , _ = require('underscore')
-  , models = require('../lib/models')
-  , Tldr = models.Tldr
-  , User = models.User
-  , DbObject = require('../lib/db')
-  , config = require('../lib/config')
-  , customUtils = require('../lib/customUtils')
-  , result = 0
-  , db = new DbObject( config.dbHost
-                     , config.dbName
-                     , config.dbPort
-                     );
-
-db.connectToDatabase(function () {
-  Tldr.find({}, function (err, tldrs) {
-    _.each(tldrs, function (tldr) {
-      result += tldr.readCount;
-    });
-
-    console.log("=== We are " + new Date() + " and the read count is: " + result);
-    process.exit(0);
-  });
-});
+echo "====================="
+date
+redis-cli get global:totalTldrReadCount
