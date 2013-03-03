d00de5ec1ba5d325cfc82e3f7a645cfe60d1d7f3
Stanislas Marion <stanislas.marion@gmail.com>
2013-03-03 19:26:36 +0100

    review of copy

--- routes/website/releaseNotes.js
+++ routes/website/releaseNotes.js
@@ -21,19 +21,18 @@ module.exports = function (req, res, next) {
   values.weeklyReleases.push({ header: 'Week from Feb 18th, 2013 to Feb 24th, 2013'
   , items: [
              "Enabled anonymous posting, you can now help the community in a more discreet way. Of course, people can still thank you for your work."
-           , "Extended the extention so that the badges appear on pinboard.in"
-           , "Embed tldrs in Reddit's comments pages"
-           , "Improved the design of the tldrs embedded in Hacker News and Reddit."
            , "Various improvements to the contribution UX: better positioned window, more intuitive behaviour of the tab command and better post contribution message."
+           , "Embed tldrs in Reddit comments pages"
+           , "Badges now appear on pinboard.in/popular and pinboard.in/recent"
+           , "Improved the design of the tldrs embedded in Hacker News and Reddit."
            ]
   });
 
   values.weeklyReleases.push({ header: 'Week from Feb 11th, 2013 to Feb 17th, 2013'
   , items: [
-             "Embed tldrs in Hacker News' comments pages."
-           , "Added ability to thank the creator of a summary."
-           , "The total number of tldrs read can be seen on the homepage."
-           , "Fixed indexing problems for Blogspot webapges: due to Blogspot's location-dependent URLs, tldrs of Blogspot articles were hidden from most users."
+             "Embed tldrs in Hacker News comments pages."
+           , "You can now thank the creator of a summary."
+           , "Fixed indexing problems for Blogspot webapges"
            , "New 'read later on Readability' button."
            , "Various bugfixes: signup page, read report email, tldr page"
            ]
