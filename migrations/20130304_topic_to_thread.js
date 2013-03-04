cf94f3d9c6971582231abadce8de82ad7cc0ccfa
Louis Chatriot <louis.chatriot@gmail.com>
2013-03-04 17:49:40 +0100

    Post part of the migration

---
+++ migrations/20130304_topic_to_thread.js
@@ -0,0 +1,90 @@
+/*
+ * All topics are now threads
+ * Date: 12/18/2012
+ *
+ */
+
+var async = require('async')
+  , _ = require('underscore')
+  , models = require('../lib/models')
+  //, Topic = models.Topic
+  , Post = models.Post
+  , DbObject = require('../lib/db')
+  , config = require('../lib/config')
+  , db = new DbObject( config.dbHost
+                     , config.dbName
+                     , config.dbPort
+                     );
+
+console.log('===== Beginning migration =====');
+console.log('Connecting to DB ' + config.dbHost + ':' + config.dbPort + '/' + config.dbName)
+
+async.waterfall([
+  function (cb) {
+    db.connectToDatabase(function() {
+      console.log('Connected');
+      cb();
+    });
+  }
+
+, function (cb) {
+    var i = 0;
+
+    Post.find({})
+      .populate('posts')
+      .exec(function(err, posts) {
+      if (err) { return cb(err); }
+
+      async.whilst(
+        function () { return i < posts.length; }
+      , function (cb) {
+          var post = post[i];
+
+          post.thread = post.topic;
+          post.save(function(err, post) {
+            if (err) { return cb(err); }
+
+            i += 1;
+            cb();
+        });
+      }
+      , cb);
+    });
+  }
+
+, function (cb) {
+    var i = 0;
+
+    Post.find({})
+      .populate('posts')
+      .exec(function(err, posts) {
+      if (err) { return cb(err); }
+
+      async.whilst(
+        function () { return i < posts.length; }
+      , function (cb) {
+          var post = post[i];
+
+          delete post.topic;
+          post.save(function(err, post) {
+            if (err) { return cb(err); }
+
+            i += 1;
+            cb();
+        });
+      }
+      , cb);
+    });
+  }
+
+], function (err) {
+    if (err) {
+      console.log('Something unexpected occured, stopped migration. ', err);
+    }
+
+    db.closeDatabaseConnection(function () {
+      console.log("Closed connection to database");
+      process.exit(0);
+    });
+  }
+);
