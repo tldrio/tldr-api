a824258e6316f2ef703339878697a7c64df3ec85
Louis Chatriot <louis.chatriot@gmail.com>
2013-03-06 23:23:24 +0100

    Able to get a delete request body

--- routes/deleteUser.js
+++ routes/deleteUser.js
@@ -1,13 +1,18 @@
 var User = require('../models/userModel');
 
 module.exports = function (req, res, next) {
-  req.user.deleteAccount(function (err) {
-    if (err) {
-      return res.send(403);
-    } else {
-      return res.send(200);
-    }
-  });
+  console.log("===================");
+  console.log("----------------");
+  console.log(req.body);
+
+  return res.send(200);
+  //req.user.deleteAccount(function (err) {
+    //if (err) {
+      //return res.send(403);
+    //} else {
+      //return res.send(200);
+    //}
+  //});
 };
 
 
