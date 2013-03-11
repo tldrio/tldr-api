b84d90f9cbc53da392fd926dd145c3a14f96acb6
Louis Chatriot <louis.chatriot@gmail.com>
2013-03-11 19:19:32 +0100

    Confirm email function now part of the usermodel

--- routes/confirmUserEmail.js
+++ routes/confirmUserEmail.js
@@ -12,48 +12,17 @@ var bunyan = require('../lib/logger').bunyan
 
 
 function confirmUserEmail (req, res, next) {
-  var confirmEmailToken = req.body.confirmEmailToken
-    , email = req.body.email;
-
-  if (!confirmEmailToken || !email) {
-    return next({ statusCode: 403, body: { message: i18n.confirmTokenOrEmailInvalid} } );
-  }
-
-  User.findOne({ email: email },  function (err, user) {
-    if (err) { return next({ statusCode: 500, body: { message: i18n.mongoInternErrGetUserEmail} } ); }
-
-    // User email is not recognised
-    if (!user) {
-      return next({ statusCode: 403, body: { message: i18n.confirmTokenOrEmailInvalid} } );
-    }
-    // If email is already confirmed send positive response
-    if (user.confirmedEmail) {
-      return res.json(200, {message: i18n.emailIsConfirmed});
+  User.confirmEmail(req.body.email, req.body.confirmEmailToken, function (err) {
+    if (err) {
+      if (err.message) {
+        return res.json(403, err);
+      } else {
+        return res.json(500, err);
+      }
     }
 
-    // User email is not confirmed - token is incorrect
-    if (user.confirmEmailToken !== confirmEmailToken) {
-      return next({ statusCode: 403, body: { message: i18n.confirmTokenOrEmailInvalid} } );
-    }
-
-    // User email is not confirmed - token is correct -> Confirm
-    var now = new Date();
-    if (!user.confirmedEmail) {
-      user.confirmedEmail = true;
-      user.confirmEmailToken = null;
-      user.save(function (err) {
-        if (err) {
-          return next({ statusCode: 500, body: { message: i18n.mongoInternErrSaveConfirmUser} } );
-        }
-
-        return res.json(200, {message: i18n.emailIsConfirmed});
-      });
-    } else {
-      return res.json(200, {message: i18n.emailIsConfirmed});
-    }
-
+    return res.json(200, {})
   });
-
 }
 
 
