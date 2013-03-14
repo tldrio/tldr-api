61877211fdd34d5ceaf61accef6287a4e812b2bf
Louis Chatriot <louis.chatriot@gmail.com>
2013-03-14 12:03:55 +0100

    Better for testing

--- routes/website/tldrEmbedTest.js
+++ routes/website/tldrEmbedTest.js
@@ -1,9 +1,3 @@
-/**
- * The tldr page, at last with the same layout as the rest of the website
- * Copyright (C) 2012 L. Chatriot, S. Marion, C. Miglietti
- * Proprietary License
-*/
-
 var _ = require('underscore')
   , Tldr = require('../../lib/models').Tldr
   , bunyan = require('../../lib/logger').bunyan
