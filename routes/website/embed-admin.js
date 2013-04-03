8ef9de0e7cd91c6b0520005c5e34492225c4294b
Louis Chatriot <louis.chatriot@gmail.com>
2013-04-03 11:45:51 +0200

    Now really readable data and sorted too

--- routes/website/embed-admin.js
+++ routes/website/embed-admin.js
@@ -16,17 +16,17 @@ module.exports = function (req, res, next) {
   beg.setDate(beg.getDate() - parseInt(daysBack, 10));
   EmbedAnalytics.find({ firstRead: { $gte: beg } }, function (err, data) {
     data = _.groupBy(data, 'hostname');
-    data = _.map(data, function (h) {
+    data = _.map(data, function (h, key) {
       return { readCount: _.reduce(_.pluck(h, 'readCount'), function(memo, n) { return memo + n; }, 0)
              , firstRead: _.min(_.pluck(h, 'firstRead'))
              , lastRead: _.max(_.pluck(h, 'lastRead'))
              , tldrsIds: _.uniq(_.pluck(h, 'tldrId'), false, function (id) { return id.toString(); })
              , pagesUrls: _.uniq(_.pluck(h, 'pageUrl'))
+             , hostname: key
              };
     });
-
-    data = _.map(_.pairs(data), function (p) { return { hostname: p[0], data: p[1] }; });
-
+    data = _.values(data);
+    data = data.sort(function (a, b) { return b.readCount - a.readCount; });
     values.data = data;
 
     res.render('website/basicLayout', { values: values
