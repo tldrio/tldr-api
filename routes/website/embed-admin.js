bd4ad3e03d32dd40df1622eae227998cae925e88
Louis Chatriot <louis.chatriot@gmail.com>
2013-04-03 12:22:06 +0200

    Embed analytics page OK

--- routes/website/embed-admin.js
+++ routes/website/embed-admin.js
@@ -1,6 +1,7 @@
 var models = require('../../lib/models')
   , EmbedAnalytics = models.EmbedAnalytics
   , _ = require('underscore')
+  , customUtils = require('../../lib/customUtils')
   ;
 
 module.exports = function (req, res, next) {
@@ -17,18 +18,29 @@ module.exports = function (req, res, next) {
   EmbedAnalytics.find({ firstRead: { $gte: beg } }, function (err, data) {
     data = _.groupBy(data, 'hostname');
     data = _.map(data, function (h, key) {
-      return { readCount: _.reduce(_.pluck(h, 'readCount'), function(memo, n) { return memo + n; }, 0)
-             , firstRead: _.min(_.pluck(h, 'firstRead'))
-             , lastRead: _.max(_.pluck(h, 'lastRead'))
-             , tldrsIds: _.uniq(_.pluck(h, 'tldrId'), false, function (id) { return id.toString(); })
-             , pagesUrls: _.uniq(_.pluck(h, 'pageUrl'))
-             , hostname: key
+      var pagesData = _.groupBy(h, 'pageUrl');
+      pagesData = _.map(pagesData, function (p, key) {
+        return { url: key
+               , readCount: _.reduce(_.pluck(p, 'readCount'), function(memo, n) { return memo + n; }, 0)
+               };
+      });
+      pagesData = _.values(pagesData);
+      pagesData = pagesData.sort(function (a, b) { return b.readCount - a.readCount; });
+
+      return { hostname: key
+             , readCount: _.reduce(_.pluck(h, 'readCount'), function(memo, n) { return memo + n; }, 0)
+             , firstRead: customUtils.dateForDisplay(_.min(_.pluck(h, 'firstRead')))
+             , lastRead: customUtils.dateForDisplay(_.max(_.pluck(h, 'lastRead')))
+             //, tldrsIds: _.uniq(_.pluck(h, 'tldrId'), false, function (id) { return id.toString(); })
+             , pagesData: pagesData
              };
     });
     data = _.values(data);
     data = data.sort(function (a, b) { return b.readCount - a.readCount; });
-    values.data = data;
 
+
+
+    values.data = data;
     res.render('website/basicLayout', { values: values
                                       , partials: partials
                                       });
